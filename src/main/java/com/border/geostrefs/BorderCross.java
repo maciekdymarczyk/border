6c689c11ea4ffc5d2540ad8a74a24c7bfa8c44a7
Maciej <maciej.dymarczyk@iterative.pl>
2020-12-22 13:04:09 +0100

    update doc

--- src/main/java/com/border/geostrefs/BorderCross.java
+++ src/main/java/com/border/geostrefs/BorderCross.java
@@ -1,15 +1,20 @@
 package com.border.geostrefs;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class BorderCross {
-
+    @ApiModelProperty(notes = "Timedate przekroczenia granicy", example = "timedate")
     private LocalDate crossTime;
+    @ApiModelProperty(notes = "Kraj z ktorego samochod wyjechal", example = "PL")
     private String from;
+    @ApiModelProperty(notes = "Kraj do ktorego samochod wjechal", example = "DE")
     private String to;
+    @ApiModelProperty(notes = "Kierowca", example = "Stig")
     private Long odometer;
+    @ApiModelProperty(notes = "Kierowca", example = "Stig")
     private String driver;
 }
--- src/main/java/com/border/geostrefs/BorderRaport.java
+++ src/main/java/com/border/geostrefs/BorderRaport.java
@@ -1,5 +1,6 @@
 package com.border.geostrefs;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -7,10 +8,17 @@ import java.time.LocalDate;
 @Data
 public class BorderRaport {
 
+    @ApiModelProperty(notes = "Kraj", example = "PL")
     private String country;
+    @ApiModelProperty(notes = "Dystans przejechany w jednym kraju", example = "12345")
     private Long distance;
+    @ApiModelProperty(notes = "Brak danych na początku do timedate", example = "timedate")
     private LocalDate entryTime;
+    @ApiModelProperty(notes = "Brak danych na koncu do timedate", example = "timedate")
     private LocalDate exitTime;
+    @ApiModelProperty(notes = "W sekundach", example = "1234")
+    private LocalDate driveTime;
+    @ApiModelProperty(notes = "exitTime - entryTime w sekundach", example = "2345")
     private LocalDate countryTime;
 
 }
