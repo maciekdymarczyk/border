ce9d1345d7baf472e1fb27cac5b270f4014ba24b
Maciej <maciej.dymarczyk@iterative.pl>
2020-12-22 13:38:17 +0100

    update doc

--- src/main/java/com/border/geostrefs/BorderRaport.java
+++ src/main/java/com/border/geostrefs/BorderRaport.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class BorderRaport {
@@ -12,13 +13,15 @@ public class BorderRaport {
     private String country;
     @ApiModelProperty(notes = "Dystans przejechany w jednym kraju", example = "12345")
     private Long distance;
-    @ApiModelProperty(notes = "Brak danych na początku do timedate", example = "timedate")
+    @ApiModelProperty(notes = "Timedate wjazdu do strefy", example = "timedate")
     private LocalDate entryTime;
-    @ApiModelProperty(notes = "Brak danych na koncu do timedate", example = "timedate")
+    @ApiModelProperty(notes = "Timedate wyjazdu ze strefy", example = "timedate")
     private LocalDate exitTime;
     @ApiModelProperty(notes = "W sekundach", example = "1234")
     private LocalDate driveTime;
     @ApiModelProperty(notes = "exitTime - entryTime w sekundach", example = "2345")
     private LocalDate countryTime;
+    @ApiModelProperty(notes = "Informacje o zakresach czasu w ktorych brakuje danych")
+    private List<NoDataBorderRaport> borderRaportNoDate;
 
 }
---
+++ src/main/java/com/border/geostrefs/NoDataBorderRaport.java
@@ -0,0 +1,15 @@
+package com.border.geostrefs;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class NoDataBorderRaport {
+    @ApiModelProperty(notes = "Brak danych data poczatkowa", example = "timedate")
+    private LocalDate entryTime;
+    @ApiModelProperty(notes = "Brak danych data koncowa", example = "timedate")
+    private LocalDate exitTime;
+
+}
